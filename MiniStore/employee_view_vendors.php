155222db9e90789294674cf57f629e73c8deb1f1
Emily Rivera <emrivera@kean.edu>
2020-11-25 22:25:58 -0500

    Add files via upload

--- MiniStore/employee_view_vendors.php
+++ MiniStore/employee_view_vendors.php
@@ -30,8 +30,31 @@
 		echo "</tr>";
 	}
 	echo "</table>";
+	$result = mysqli_query($conn,"SELECT * FROM VENDOR");
 
-	mysqli_close($conn);
+echo '
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBrmJjh1AveghA62RvRdNXtULNYda3wrLc&callback=initMap&libraries=&v=weekly" defer> </script>
+
+	<style type="text/css">
+		#map { height: 400px; width: 100%; }
+	</style>
+	
+	<script>
+		function initMap() {';
+
+	echo 'const map = new google.maps.Map(document.getElementById("map"), { zoom : 4, center : {lat: 37.8, lng: -96}});';
+	echo 'const markers = [];';
+	while($row = mysqli_fetch_array($result))
+	{	
+		echo '
+		markers.push(new google.maps.Marker({ position: { lat: '.$row['latitude'].', lng: '.$row['Longitude'].' }, label: String('. strval($row['vendor_id']).'), }));';
+		echo 'markers[markers.length-1].setMap(map);';
+	}
+     		echo'}
+	</script>
+    	<!--The div element for the map -->
+    	<div id="map"></div>
+  ';
 ?>
 <form name='input' action='employee_homepage.php' method='post'>
 	<input type='submit' value='Employee Homepage'>	
@@ -39,5 +62,4 @@
 <form name='input' action='phase2.html' method='post'>
 	<input type='submit' value='Phase2 Homepage'>	
 </form>
-
 </html>
